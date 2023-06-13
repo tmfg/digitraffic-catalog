3fc29222fbb78620584dcbb0f513e2881f8a00ff
teijosol <teijo.ronka@solita.fi>
2023-06-13 15:53:27 +0300

    install dcat & scheming

--- docker/ckan/setup/install_extensions.sh
+++ docker/ckan/setup/install_extensions.sh
@@ -16,6 +16,9 @@ pip_install() {
 
 # install requirements
 pip_install "${EXT_DIR}/ckanext-digitraffic_theme/dev-requirements.txt"
+pip install -r "https://raw.githubusercontent.com/ckan/ckanext-dcat/v0.0.6/requirements.txt"
 
 # install extensions
 pip install -e "${EXT_DIR}/ckanext-digitraffic_theme"
+pip install -e "git+https://github.com/ckan/ckanext-scheming.git@master#egg=ckanext-scheming"
+pip install -e "git+https://github.com/ckan/ckanext-dcat.git@v0.0.6#egg=ckanext-dcat"
