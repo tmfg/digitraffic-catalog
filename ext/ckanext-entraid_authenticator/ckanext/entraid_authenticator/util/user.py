035f9adb987a96217b0c814ef80d98e911cbb315
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-07-30 13:43:33 +0300

    DPO-2599 Update install workflow with new extension

--- ext/ckanext-entraid_authenticator/ckanext/entraid_authenticator/util/user.py
+++ ext/ckanext-entraid_authenticator/ckanext/entraid_authenticator/util/user.py
@@ -5,10 +5,11 @@ import uuid
 
 def create_user_from_graph_api_info(user_info: GraphApiUserInfo) -> User:
     return User(
+        # user's OID in Entra
         id=user_info["id"],
         email=user_info.get("mail", ""),
-        # username must be unique
+        # username must be unique and there is no guarantee that user's name and/or surname are available for example
         name=uuid.uuid4(),
-        # this is the displayed name
+        # this is the displayed name, can be edited by user
         fullname=f"{user_info.get('givenName', '')} {user_info.get('surname', '')}".strip(),
     )
