7b02706abc44c4c7a9da3756da0a0b56345b0a5b
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-07-31 09:03:14 +0300

    rename env vars

--- ext/ckanext-entraid_authenticator/ckanext/entraid_authenticator/app_config.py
+++ ext/ckanext-entraid_authenticator/ckanext/entraid_authenticator/app_config.py
@@ -3,10 +3,10 @@ import os
 
 load_dotenv()
 
-AUTHORITY = os.getenv("AUTHORITY")
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-HOST = os.getenv("HOST")
+AUTHORITY = os.getenv("ENTRA_AUTHORITY")
+CLIENT_ID = os.getenv("ENTRA_CLIENT_ID")
+CLIENT_SECRET = os.getenv("ENTRA_CLIENT_SECRET")
+HOST = os.getenv("CKAN_SITE_URL")
 REDIRECT_PATH = "/user/login/auth_redirect"
 GRAPH_ENDPOINT = "https://graph.microsoft.com/v1.0/me"
 SCOPE = ["User.Read"]
