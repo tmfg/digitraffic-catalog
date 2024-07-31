8bd37c15c2f630bf9bb78d53797ba25c8b7e3f18
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-07-31 14:30:44 +0300

    DPO-2599 Try to get displayable name from all possible fields in graph api

--- ext/ckanext-entraid_authenticator/ckanext/entraid_authenticator/util/user.py
+++ ext/ckanext-entraid_authenticator/ckanext/entraid_authenticator/util/user.py
@@ -3,6 +3,10 @@ from ckanext.entraid_authenticator.model.entra_id import GraphApiUserInfo
 import uuid
 
 
+def get_full_name_from_user_info(user_info: GraphApiUserInfo) -> str:
+    return f"{'' if user_info.get('givenName') is None else user_info.get('givenName')} {'' if user_info.get('surname') is None else user_info.get('surname')}"
+
+
 def create_user_from_graph_api_info(user_info: GraphApiUserInfo) -> User:
     return User(
         # user's OID in Entra
@@ -11,5 +15,5 @@ def create_user_from_graph_api_info(user_info: GraphApiUserInfo) -> User:
         # username must be unique and there is no guarantee that user's name and/or surname are available for example
         name=uuid.uuid4(),
         # this is the displayed name, can be edited by user
-        fullname=f"{'' if user_info.get('givenName') is None else user_info.get('givenName')} {'' if user_info.get('surname') is None else user_info.get('surname')}".strip(),
+        fullname=f"{user_info.get("displayName") if user_info.get("displayName") is not None else get_full_name_from_user_info(user_info)}".strip(),
     )
