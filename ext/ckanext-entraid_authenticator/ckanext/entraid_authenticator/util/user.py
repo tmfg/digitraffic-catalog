42e8246bce7a04f44e505ef941e5568a6b4968a5
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-08-06 10:07:39 +0300

    DPO-2599 Update README

--- ext/ckanext-entraid_authenticator/ckanext/entraid_authenticator/util/user.py
+++ ext/ckanext-entraid_authenticator/ckanext/entraid_authenticator/util/user.py
@@ -12,7 +12,7 @@ def create_user_from_graph_api_info(user_info: GraphApiUserInfo) -> User:
         # user's OID in Entra
         id=user_info["id"],
         email=user_info.get("mail", ""),
-        # username must be unique and there is no guarantee that user's name and/or surname are available for example
+        # CKAN username must be unique and there is no guarantee that fields other than user OID are available
         name=uuid.uuid4(),
         # this is the displayed name, can be edited by user
         fullname=f"{user_info.get('displayName') if user_info.get('displayName') is not None else get_full_name_from_user_info(user_info)}".strip(),
