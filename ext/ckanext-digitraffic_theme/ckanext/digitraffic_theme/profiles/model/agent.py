56338128129c2bb2277791782a067b56d359db49
Joonas-M-S <joonas.mikkonen@solita.fi>
2024-08-21 16:17:58 +0300

    Metadata changes

--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/profiles/model/agent.py
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/profiles/model/agent.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from rdflib import Literal, URIRef, FOAF
+from rdflib import Literal, URIRef, FOAF, RDF
 from ckanext.digitraffic_theme.profiles.model.class_instance import ClassInstance
 
 
@@ -10,8 +10,10 @@ class Agent(ClassInstance):
     def __init__(self, iri: str, name: str):
         super().__init__(iri)
         self.name = Literal(name)
+        self.type = FOAF.Organization
 
     def predicate_objects(self):
         return [
+            (RDF.type, self.type),
             (FOAF.name, self.name)
         ]
--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/profiles/model/mobility_data.py
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/profiles/model/mobility_data.py
@@ -21,7 +21,7 @@ class MobilityData:
     title: Literal
     publisher: Agent
 
-    def __init__(self, dataset_dict: dict[str, Any]):
+    def __init__(self, dataset_dict: dict[str, Any], dataset_ref: URIRef):
         self.author = dataset_dict["author"]
         self.description = Literal(dataset_dict["notes"])
         self.distribution = [Distribution(dist) for dist in dataset_dict["resources"]]
@@ -30,5 +30,5 @@ class MobilityData:
         self.mobility_theme_sub = MobilityThemeSub(dataset_dict["mobility_theme_sub"])
         #self.spatial = Location(dataset_dict["gazetteer"], dataset_dict["geographic_identifier"])
         self.title = Literal(dataset_dict["name"])
-        self.publisher = Agent("TODO_foo", dataset_dict["publisher_name"])
+        self.publisher = Agent(str(dataset_ref) + '/publisher', dataset_dict["publisher_name"])
 
