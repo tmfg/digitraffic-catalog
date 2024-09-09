6344b1f346bfd197c394cb19b5ebbbf0af8aa77b
Joonas-M-S <joonas.mikkonen@solita.fi>
2024-09-09 18:10:08 +0300

    CSS and js bundling configurations. Modify the select element used by ckanext-scheming

--- ext/ckanext-digitraffic_theme/rollup.config.js
+++ ext/ckanext-digitraffic_theme/rollup.config.js
@@ -1,24 +1,66 @@
 import styles from "rollup-plugin-styler";
 import copy from 'rollup-plugin-copy'
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import image from '@rollup/plugin-image';
+import cssnano from "cssnano";
+import autoprefixer from "autoprefixer";
 
-export default {
-    input: "bundleCSS.js",
-    output: {
-        file: "bundle.js",
-        assetFileNames: "[name][extname]",
-    },
-
-    plugins: [
+const inputs = {
+    'css/digitraffic-theme': "ckanext/digitraffic_theme/resources/js/digitrafficTheme.js",
+    'js/digitrafficWebComponents': "ckanext/digitraffic_theme/resources/js/digitrafficWebComponents.js"
+}
+export default Object.entries(inputs).map(([name, file]) => {
+    const isOutputJs = name.startsWith('js/')
+    const cssThemePlugins = [
         copy({
             targets: [
                 {src: './node_modules/ckan/ckan/public/base/*', dest: './tmp/ckan_base'},
-                {src: './ckanext/digitraffic_theme/resources/sass/override_ckan/_variables.scss', dest: './tmp/ckan_base/scss'}
+                {
+                    src: './ckanext/digitraffic_theme/resources/sass/override_ckan/_variables.scss',
+                    dest: './tmp/ckan_base/scss'
+                }
             ],
             hook: 'buildStart'
         }),
+        //image(),
         styles({
             mode: "extract",
+            url: {
+                //inline: true
+                publicPath: "../../assets",
+                assetDir: "ckanext/digitraffic_theme/public"
+            },
+            plugins: [
+                autoprefixer,
+                cssnano
+            ],
+            exclude: [
+                "ckanext/digitraffic_theme/resources/js/digitrafficWebComponents.js"
+            ]
         }),
+    ]
+    const jsPlugins = [
+        nodeResolve({
+            exportConditions: ["node", "default", "module", "import", "require"],
+            preferBuiltins: true
+        }),
+        terser()
+    ]
+    return {
+        input: {
+            [name]: file
+        },
+        output: isOutputJs ? {
+            inlineDynamicImports: true,
+            dir: ".",
+            entryFileNames: "ckanext/digitraffic_theme/assets/[name].js",
+            format: "es"
+        } : {
+            dir: ".",
+            assetFileNames: "ckanext/digitraffic_theme/assets/[name][extname]",
+        },
 
-    ],
-};
+        plugins: isOutputJs ? jsPlugins : cssThemePlugins,
+    }
+});
