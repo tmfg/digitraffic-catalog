93ad6cd27057d57f04e9a725b255265d9339427c
Joonas-M-S <joonas.mikkonen@solita.fi>
2024-09-25 14:48:25 +0300

    Custom dropdown and styling

--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/resources/ts/ckan.d.ts
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/resources/ts/ckan.d.ts
@@ -1,10 +1,11 @@
-import type jQuery from '@types/jquery'
+import $ from 'ckan/ckan/public/base/vendor/jquery'
 
 declare module 'ckan' {
   export type Module = {
-    initialize: () => void
+    initialize: () => void,
+    [key: string]: unknown
   }
   // The properties function properties are actually (jQuery: jQuery, translate, i18n), but use of
   // 'translate' and 'i18n' are deprecated
-  export function module(name: string, properties: ((jQuery: jQuery) => Module) | Module): void;
+  export function module(name: string, properties: ((jQuery: $) => Module) | Module): void;
 }
--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/resources/ts/main.ts
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/resources/ts/main.ts
@@ -1,2 +1,6 @@
 import './modules/top-navigaiton'
-import './modules/app-navigation'
+import './modules/user-actions'
+
+jQuery( function() {
+  $(".js-disabled").removeClass("js-disabled")
+})
