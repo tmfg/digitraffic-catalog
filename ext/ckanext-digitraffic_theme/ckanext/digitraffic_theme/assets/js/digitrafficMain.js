151441520e7b0f2ba351bebfbcc25fb9b95c1613
Joonas-M-S <joonas.mikkonen@solita.fi>
2024-09-26 10:45:39 +0300

    User actions navigation works

--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/assets/js/digitrafficMain.js
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/assets/js/digitrafficMain.js
@@ -1518,18 +1518,113 @@ ckan.module('digitraffic_theme_user_actions', function ($) {
     return {
         initialize: function () {
             $.proxyAll(this, /_on/);
-            this.el.on('click', this._onClick);
+            this._getMenuController().on('click', this._onMenuControllerClick);
+            this._getMenuController().on('keydown', this._onMenuControllerKeyDown);
+            this._getMenu().on('keydown', this._onMenuKeyDown);
         },
-        _onClick: function (event) {
-            console.log(this);
-            if (this._isListOpen()) {
-                this._closeList();
+        _setActiveDescendant: function (el) {
+            this._getMenu().attr("aria-activedescendant", el.attr('id'));
+        },
+        _removeActiveDescendant: function () {
+            this._getMenu().removeAttr("aria-activedescendant");
+        },
+        _onMenuControllerClick: function (event) {
+            this._toggleList();
+        },
+        _onMenuControllerKeyDown: function (event) {
+            const { key } = event;
+            switch (key) {
+                case ' ':
+                case 'Enter':
+                    event.preventDefault();
+                    this._toggleList();
+                    break;
+                case 'ArrowDown':
+                    event.preventDefault();
+                    this._focus('first');
+                    break;
             }
-            else {
-                this._openList();
+        },
+        _onMenuKeyDown: function (event) {
+            const { key } = event;
+            switch (key) {
+                case 'Escape':
+                    event.preventDefault();
+                    this._closeList();
+                    this._focus('menuController');
+                    break;
+                case 'ArrowDown':
+                    event.preventDefault();
+                    this._focus('next');
+                    break;
+                case 'ArrowUp':
+                    event.preventDefault();
+                    this._focus('previous');
             }
         },
         _expandedClass: "expanded",
+        _focus: function (elementKey) {
+            let el;
+            const currentlyFocusedElement = this.el.find(':focus');
+            const isFocusOnMenu = !!this._getMenu().has(currentlyFocusedElement);
+            const isFocusOnLastMenuItem = isFocusOnMenu && this._getMenu().find('a:last')[0] === currentlyFocusedElement[0];
+            const isFocusOnFirstMenuItem = isFocusOnMenu && this._getMenu().find('a:first')[0] === currentlyFocusedElement[0];
+            switch (elementKey) {
+                case "first":
+                    el = this._getMenu().find('a:first');
+                    break;
+                case "menuController":
+                    el = this._getMenuController();
+                    break;
+                case "next":
+                    if (isFocusOnMenu) {
+                        if (isFocusOnLastMenuItem) {
+                            return;
+                        }
+                        else {
+                            const menuElements = this._getMenu().find('a');
+                            const nextElement = menuElements.filter((index) => index > 0 && menuElements[index - 1] === currentlyFocusedElement[0]);
+                            el = nextElement;
+                        }
+                    }
+                    else {
+                        el = this._getMenu().find('a:first');
+                    }
+                    break;
+                case "previous":
+                    if (isFocusOnMenu) {
+                        if (isFocusOnFirstMenuItem) {
+                            return;
+                        }
+                        else {
+                            const menuElements = this._getMenu().find('a');
+                            const previousElement = menuElements.filter((index) => index < (menuElements.length - 1) && menuElements[index + 1] === currentlyFocusedElement[0]);
+                            el = previousElement;
+                        }
+                    }
+                    else {
+                        el = this._getMenu().find('a:first');
+                    }
+                    break;
+            }
+            el.trigger('focus');
+            if (elementKey === "menuController") {
+                this._removeActiveDescendant();
+            }
+            else {
+                this._setActiveDescendant(el);
+            }
+        },
+        _toggleList: function () {
+            if (this._isListOpen()) {
+                this._closeList();
+                this._focus('menuController');
+            }
+            else {
+                this._openList();
+                this._focus('first');
+            }
+        },
         _isListOpen: function () {
             const list = $("#user-action-list");
             return list.hasClass(this._expandedClass);
@@ -1545,6 +1640,12 @@ ckan.module('digitraffic_theme_user_actions', function ($) {
             const list = $("#user-action-list");
             list.addClass(this._expandedClass);
             button.attr("aria-expanded", "true");
+        },
+        _getMenuController() {
+            return $("#user-action-select");
+        },
+        _getMenu() {
+            return $("#user-action-list");
         }
     };
 });
