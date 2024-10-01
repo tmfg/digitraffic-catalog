2640455b876aab8985653ee294dbfe99066a654b
Joonas-M-S <joonas.mikkonen@solita.fi>
2024-10-01 18:36:53 +0300

    Add footer

--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/main.ts
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/main.ts
@@ -1,7 +1,13 @@
 import './modules/top-navigaiton'
 import './modules/app-navigation'
 import './modules/user-actions'
+import { createIcons, ExternalLink, Facebook, Twitter, Instagram, Youtube, Linkedin } from 'lucide'
 
 jQuery( function() {
   $(".js-disabled").removeClass("js-disabled")
+  createIcons({
+    icons: {
+      ExternalLink, Facebook, Twitter, Instagram, Youtube, Linkedin
+    }
+  })
 })
