084b92586a9db18e9e6343d314640244b5da35e3
Joonas-M-S <joonas.mikkonen@solita.fi>
2024-10-16 10:38:31 +0300

    Refactor to more functional direciton

---
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/tests/profiles/graph_modifiers/test_adder.py
@@ -0,0 +1,56 @@
+from rdflib import Graph, URIRef, Literal
+from rdflib.namespace import FOAF, DCTERMS
+
+from ckanext.digitraffic_theme.profiles.graph_modifiers.adder import (
+    add_class_instance_with_children,
+    add_class_instance_to_graph,
+    add_literal_to_graph,
+    add_vocabulary_to_graph
+)
+from ckanext.digitraffic_theme.profiles.model.agent import Agent
+from ckanext.digitraffic_theme.profiles.model.mobility_theme import MobilityTheme
+from ckanext.digitraffic_theme.profiles.rdf.mobility_dcat_ap import MOBILITYDCATAP
+
+
+def test_add_class_instance_with_children_adds_correctly():
+    g = Graph()
+    subject = URIRef('example.com/foo')
+    agent_iri = URIRef('example.com/agent')
+    agent = Agent(agent_iri, "foo")
+
+    add_class_instance_with_children(g, subject, DCTERMS.publisher, agent)
+
+    assert (subject, DCTERMS.publisher, agent_iri) in g
+    for p, o in agent.predicate_objects():
+        assert (agent_iri, p, o) in g
+
+
+def test_add_class_instance_to_graph_adds_correctly():
+    g = Graph()
+    subject = URIRef('example.com/foo')
+    agent_iri = URIRef('example.com/agent')
+    agent = Agent(agent_iri, "foo")
+
+    add_class_instance_to_graph(g, subject, DCTERMS.publisher, agent)
+
+    assert (subject, DCTERMS.publisher, agent_iri) in g
+
+
+def test_add_literal_to_graph_adds_correctly():
+    g = Graph()
+    foo_name = "bar"
+    subject = URIRef('example.com/foo')
+
+    add_literal_to_graph(g, subject, FOAF.name, Literal(foo_name))
+
+    assert (subject, FOAF.name, Literal(foo_name)) in g
+
+def test_add_vocabulary_to_graph_adds_correctly():
+    g = Graph()
+    subject = URIRef('example.com/foo')
+    mobility_theme = MobilityTheme("https://w3id.org/mobilitydcat-ap/mobility-theme/waterways-and-water-bodies")
+
+    add_vocabulary_to_graph(g, subject, MOBILITYDCATAP.mobilityTheme, mobility_theme)
+
+    assert (subject, MOBILITYDCATAP.mobilityTheme, mobility_theme.iri) in g
+
