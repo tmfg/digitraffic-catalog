994d2cbf4fda39ed3add5eedfb8c78cf0a3232cc
Joonas-M-S <joonas.mikkonen@solita.fi>
2024-10-29 15:50:01 +0200

    Implement own select-switch module

--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/main.ts
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/main.ts
@@ -1,6 +1,7 @@
 import './modules/top-navigaiton'
 import './modules/app-navigation'
 import './modules/user-actions'
+import './modules/select-switch'
 import { createIcons, ExternalLink, User, Menu, Globe, ChevronDown, ChevronUp, Facebook, Twitter, Instagram, Youtube, Linkedin } from 'lucide'
 
 jQuery( function() {
