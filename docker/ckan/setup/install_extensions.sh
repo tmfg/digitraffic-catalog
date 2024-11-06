432c2347428574433b2d71fbb4ed682c9f41164f
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-11-06 15:29:51 +0200

    DPO-2862 fork fluent as digitraffic_fluent

--- docker/ckan/setup/install_extensions.sh
+++ docker/ckan/setup/install_extensions.sh
@@ -19,12 +19,13 @@ pip_install() {
 pip install -e "${EXT_DIR}/ckanext-digitraffic_theme"
 pip install -e "${EXT_DIR}/ckanext-entraid_authenticator"
 pip install -e "${EXT_DIR}/ckanext-digitraffic_validators"
+pip install -e "${EXT_DIR}/ckanext-digitraffic_fluent"
 pip install -e "git+https://github.com/ckan/ckanext-dcat.git@v1.7.0#egg=ckanext-dcat"
 pip install -e "git+https://github.com/ckan/ckanext-scheming.git@release-3.0.0#egg=ckanext-scheming"
-pip install -e "git+https://github.com/ckan/ckanext-fluent.git#egg=ckanext-fluent"
 
 # install requirements
 pip_install "${EXT_DIR}/ckanext-digitraffic_theme/dev-requirements.txt"
 pip_install "${EXT_DIR}/ckanext-entraid_authenticator/requirements.txt"
 pip_install "${EXT_DIR}/ckanext-digitraffic_validators/requirements.txt"
+pip_install "${EXT_DIR}/ckanext-digitraffic_fluent/requirements.txt"
 pip install -r src/ckanext-dcat/requirements.txt
---
+++ ext/ckanext-digitraffic_fluent/ckanext/fluent/helpers.py
@@ -0,0 +1,63 @@
+from ckan.lib.i18n import get_available_locales
+
+from ckanext.scheming.helpers import scheming_language_text
+
+
+def fluent_form_languages(field=None, entity_type=None, object_type=None, schema=None):
+    """
+    Return a list of language codes for this form (or form field)
+
+    1. return field['form_languages'] if it is defined
+    2. return schema['form_languages'] if it is defined
+    3. get schema from entity_type + object_type then
+       return schema['form_languages'] if they are defined
+    4. return languages from site configuration
+    """
+    if field and "form_languages" in field:
+        return field["form_languages"]
+    if schema and "form_languages" in schema:
+        return schema["form_languages"]
+    if entity_type and object_type:
+        # late import for compatibility with older ckanext-scheming
+        from ckanext.scheming.helpers import scheming_get_schema
+
+        schema = scheming_get_schema(entity_type, object_type)
+        if schema and "form_languages" in schema:
+            return schema["form_languages"]
+
+    langs = []
+    for l in get_available_locales():
+        if l.language not in langs:
+            langs.append(l.language)
+    return langs
+
+
+def fluent_alternate_languages(field=None, schema=None):
+    """
+    Return a dict of alternates acceptable as replacements for
+    required languages, as given in the field or schema.
+
+    e.g. {'en': ['en-GB']}
+    """
+    if field and "alternate_languages" in field:
+        return field["alternate_languages"]
+    if schema and "alternate_languages" in schema:
+        return schema["alternate_languages"]
+    return {}
+
+
+def fluent_form_label(field, lang):
+    """
+    Return a label for the input field for the given language
+
+    If the field has a fluent_form_label defined the label will
+    be taken from there.  If a matching label can't be found
+    this helper will return the language code in uppercase and
+    the standard label.
+    """
+    form_label = field.get("fluent_form_label", {})
+
+    if lang in form_label:
+        return scheming_language_text(form_label[lang])
+
+    return lang.upper() + " " + scheming_language_text(field["label"])
