92367294ac1ee606fe510a9f7a3e4f4fb3e9659d
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-11-27 15:17:52 +0200

    DPO-2645 add mobilitydcatap:Assessment

---
+++ dcat_schema_transpiler/dcat_schema_transpiler/ckan_schema/mobility_dcat_ap_converter/classes/assessment.py
@@ -0,0 +1,56 @@
+from ckan_schema.mobility_dcat_ap_converter.range_value_converter import (
+    RangeValueConverter,
+)
+from rdflib import DCTERMS, Dataset
+
+from mobility_dcat_ap.dataset import OA
+from rdfs.rdfs_class import RDFSClass
+from rdfs.rdfs_property import RDFSProperty
+
+
+class Assessment(RangeValueConverter):
+
+    mandatory_properties = {}
+
+    recommended_properties = {}
+
+    optional_properties = {DCTERMS.issued, OA.hasBody}
+
+    def __init__(self, clazz: RDFSClass):
+        super().__init__(clazz)
+
+    def ckan_field(self, p: RDFSProperty, pointer: str | None = None) -> str:
+        mappings = {DCTERMS.issued: "assessment_date", OA.hasBody: "assessment_result"}
+
+        field_name = mappings.get(p.iri)
+
+        if field_name is not None:
+            return field_name
+        else:
+            raise ValueError(
+                f"A mapping was not found between the class {self.clazz.iri} property {p.iri} and CKAN datamodel"
+            )
+
+    def get_schema(self, ds: Dataset, clazz_p: RDFSProperty, is_required: bool = False):
+        print("hello4")
+        class_properties = self.optional_properties
+
+        if clazz_p.iri in class_properties:
+            if clazz_p.is_iri(DCTERMS.issued):
+                return super().get_schema(ds, clazz_p, is_required)
+            if clazz_p.is_iri(OA.hasBody):
+                return super().get_schema(ds, clazz_p, is_required)
+        return None
+
+    def get_label(self, p: RDFSProperty, ds: Dataset):
+        if p.is_iri(DCTERMS.issued):
+            return "Assessment date"
+        if p.is_iri(OA.hasBody):
+            return "Assessment result"
+        return super().get_label(p, ds)
+
+    def get_range_value(self, ds: Dataset, clazz_p: RDFSProperty) -> RDFSClass | None:
+        return super().get_range_value(ds, clazz_p)
+
+    def is_property_required(self, property: RDFSProperty) -> bool:
+        return property.iri in Assessment.mandatory_properties
