eacab98ab5800a793e61ea190c969ecf838450ec
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-12-05 14:46:56 +0200

    DPO-2859 rewrite language menu without using <select> and <option>

---
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/modules/language-menu.ts
@@ -0,0 +1,42 @@
+import type * as ckan from "../ckan";
+
+ckan.module("digitraffic_theme_language_menu", function ($) {
+  return {
+    initialize: function () {
+      const customDropdown = document.querySelector(
+        ".custom-language-dropdown"
+      );
+      const customOptions = document.querySelectorAll(
+        ".custom-language-option"
+      );
+      const hiddenInput = document.getElementById(
+        "language-option-hidden"
+      ) as HTMLInputElement;
+      const form = document.querySelector(".lang-select") as HTMLFormElement;
+      if (customDropdown) {
+        customDropdown.addEventListener("click", function () {
+          this.classList.toggle("open");
+        });
+      }
+
+      customOptions.forEach(function (option) {
+        option.addEventListener("click", function (e) {
+          e.stopPropagation();
+          const value = option.getAttribute("data-value");
+          if (hiddenInput && value) {
+            hiddenInput.value = value;
+          }
+          if (form) {
+            form.submit();
+          }
+        });
+      });
+
+      document.addEventListener("click", function (e) {
+        if (customDropdown && !customDropdown.contains(e.target as Node)) {
+          customDropdown.classList.remove("open");
+        }
+      });
+    },
+  };
+});
