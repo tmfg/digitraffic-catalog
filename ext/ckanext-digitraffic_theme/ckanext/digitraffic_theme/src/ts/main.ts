5474e6a347ea14d6e1fcda7b9d9da8166f457d92
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-12-05 14:46:56 +0200

    DPO-2859 rewrite language menu without using <select> and <option>

--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/main.ts
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/main.ts
@@ -1,15 +1,39 @@
-import './modules/top-navigaiton'
-import './modules/app-navigation'
-import './modules/user-actions'
-import './modules/select-switch'
-import './modules/dataset-form'
-import { createIcons, ExternalLink, User, Menu, Globe, ChevronDown, ChevronUp, Facebook, Twitter, Instagram, Youtube, Linkedin } from 'lucide'
+import "./modules/top-navigaiton";
+import "./modules/app-navigation";
+import "./modules/user-actions";
+import "./modules/select-switch";
+import "./modules/dataset-form";
+import "./modules/language-menu";
+import {
+  createIcons,
+  ExternalLink,
+  User,
+  Menu,
+  Globe,
+  ChevronDown,
+  ChevronUp,
+  Facebook,
+  Twitter,
+  Instagram,
+  Youtube,
+  Linkedin,
+} from "lucide";
 
-jQuery( function() {
-  $(".js-disabled").removeClass("js-disabled")
+jQuery(function () {
+  $(".js-disabled").removeClass("js-disabled");
   createIcons({
     icons: {
-      ExternalLink, User, Menu, Globe, ChevronDown, ChevronUp, Facebook, Twitter, Instagram, Youtube, Linkedin
-    }
-  })
-})
+      ExternalLink,
+      User,
+      Menu,
+      Globe,
+      ChevronDown,
+      ChevronUp,
+      Facebook,
+      Twitter,
+      Instagram,
+      Youtube,
+      Linkedin,
+    },
+  });
+});
