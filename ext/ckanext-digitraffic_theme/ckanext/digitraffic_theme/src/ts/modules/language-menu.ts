2f0fcc07c0929ca290f2faa4ce4fdb0ef3916a36
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-12-10 11:33:39 +0200

    DPO-2859 clean up

--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/modules/language-menu.ts
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/modules/language-menu.ts
@@ -3,40 +3,56 @@ import type * as ckan from "../ckan";
 ckan.module("digitraffic_theme_language_menu", function ($) {
   return {
     initialize: function () {
-      const customDropdown = document.querySelector(
+      const languageDropdown = document.querySelector(
         ".custom-language-dropdown"
       );
-      const customOptions = document.querySelectorAll(
+      const languageOptions = document.querySelectorAll(
         ".custom-language-option"
       );
-      const hiddenInput = document.getElementById(
+      const formInput = document.getElementById(
         "language-option-hidden"
       ) as HTMLInputElement;
       const form = document.querySelector(".lang-select") as HTMLFormElement;
-      if (customDropdown) {
-        customDropdown.addEventListener("click", function (e) {
-          const target = e.currentTarget as HTMLElement;
-          target.classList.toggle("open");
+
+      if (languageDropdown) {
+        languageDropdown.addEventListener("click", function (e) {
+          languageDropdown.classList.toggle("open");
+        });
+        languageDropdown.addEventListener("keydown", function (e) {
+          if (
+            e instanceof KeyboardEvent &&
+            (e.key === "Enter" || e.key === " ")
+          ) {
+            e.preventDefault();
+            languageDropdown.classList.toggle("open");
+          }
         });
       }
 
-      customOptions.forEach(function (option) {
+      languageOptions.forEach(function (option) {
         option.addEventListener("click", function (e) {
-          e.stopPropagation();
           const value = option.getAttribute("data-value");
-          if (hiddenInput && value) {
-            hiddenInput.value = value;
+          if (formInput && value) {
+            formInput.value = value;
           }
           if (form) {
             form.submit();
           }
         });
-      });
-
-      document.addEventListener("click", function (e) {
-        if (customDropdown && !customDropdown.contains(e.target as Node)) {
-          customDropdown.classList.remove("open");
-        }
+        option.addEventListener("keydown", function (e) {
+          if (
+            e instanceof KeyboardEvent &&
+            (e.key === "Enter" || e.key === " ")
+          ) {
+            const value = option.getAttribute("data-value");
+            if (formInput && value) {
+              formInput.value = value;
+            }
+            if (form) {
+              form.submit();
+            }
+          }
+        });
       });
     },
   };
