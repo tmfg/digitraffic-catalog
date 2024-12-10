cb892105ce6bcae096224b7e46dbcdb9b9bfadb4
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-12-10 16:09:30 +0200

    DPO-2859 use jquery objects

--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/modules/language-menu.ts
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/src/ts/modules/language-menu.ts
@@ -1,59 +1,85 @@
 import type * as ckan from "../ckan";
+import { initialize } from "../module-constructs/module";
+
+const LanguageMenu: ckan.Module<HTMLDivElement> = {
+  initialize(this) {
+    initialize.apply(this);
+    const form = this._getForm();
+    const formInput = this._getFormInput();
+    const languageDropdown = this._getLanguageDropdown();
+    const languageOptions = this._getLanguageOptions();
+
+    languageDropdown.on("click", this._toggleLanguageDropdownMouseOpen);
+    languageDropdown.on("keydown", this._toggleLanguageDropdownKeyboardOpen);
+
+    languageOptions.each((_index: number, element: JQuery<HTMLElement>) => {
+      const option = $(element);
+      option.on("click", () => this._submitFormMouse(option, formInput, form));
+      option.on("keydown", (e: JQuery.TriggeredEvent) =>
+        this._submitFormKeyboard(e, option, formInput, form)
+      );
+    });
+  },
+
+  _toggleLanguageDropdownMouseOpen(e: JQuery.TriggeredEvent) {
+    if (e.target) {
+      (e.target as HTMLElement).classList.toggle("open");
+    }
+  },
+  _toggleLanguageDropdownKeyboardOpen(e: JQuery.TriggeredEvent) {
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
+      if (e.target) {
+        (e.target as HTMLElement).classList.toggle("open");
+      }
+    }
+  },
+
+  _submitFormMouse(
+    languageOption: JQuery<HTMLElement>,
+    formInput: JQuery<HTMLElement>,
+    form: JQuery<HTMLFormElement>
+  ) {
+    const value = languageOption.attr("data-value");
+    if (formInput && value) {
+      formInput.val(value);
+    }
+    if (form) {
+      form.trigger("submit");
+    }
+  },
+
+  _submitFormKeyboard(
+    event: JQuery.TriggeredEvent,
+    languageOption: JQuery<HTMLElement>,
+    formInput: JQuery<HTMLElement>,
+    form: JQuery<HTMLFormElement>
+  ) {
+    if (event.key === "Enter" || event.key === " ") {
+      const value = languageOption.attr("data-value");
+      if (formInput && value) {
+        formInput.val(value);
+      }
+      if (form) {
+        form.trigger("submit");
+      }
+    }
+  },
+
+  _getForm(): JQuery<HTMLElement> {
+    return this.$("#language-menu-form");
+  },
+  _getFormInput(): JQuery<HTMLElement> {
+    return this.$("#language-option-hidden");
+  },
+  _getLanguageDropdown(): JQuery<HTMLElement> {
+    return this.$(".custom-language-dropdown");
+  },
+  _getLanguageOptions(): JQuery<HTMLElement> {
+    return this.$(".custom-language-option");
+  },
+};
 
 ckan.module("digitraffic_theme_language_menu", function ($) {
-  return {
-    initialize: function () {
-      const languageDropdown = document.querySelector(
-        ".custom-language-dropdown"
-      );
-      const languageOptions = document.querySelectorAll(
-        ".custom-language-option"
-      );
-      const formInput = document.getElementById(
-        "language-option-hidden"
-      ) as HTMLInputElement;
-      const form = document.querySelector(".lang-select") as HTMLFormElement;
-
-      if (languageDropdown) {
-        languageDropdown.addEventListener("click", function (e) {
-          languageDropdown.classList.toggle("open");
-        });
-        languageDropdown.addEventListener("keydown", function (e) {
-          if (
-            e instanceof KeyboardEvent &&
-            (e.key === "Enter" || e.key === " ")
-          ) {
-            e.preventDefault();
-            languageDropdown.classList.toggle("open");
-          }
-        });
-      }
-
-      languageOptions.forEach(function (option) {
-        option.addEventListener("click", function (e) {
-          const value = option.getAttribute("data-value");
-          if (formInput && value) {
-            formInput.value = value;
-          }
-          if (form) {
-            form.submit();
-          }
-        });
-        option.addEventListener("keydown", function (e) {
-          if (
-            e instanceof KeyboardEvent &&
-            (e.key === "Enter" || e.key === " ")
-          ) {
-            const value = option.getAttribute("data-value");
-            if (formInput && value) {
-              formInput.value = value;
-            }
-            if (form) {
-              form.submit();
-            }
-          }
-        });
-      });
-    },
-  };
+  return LanguageMenu;
 });
