ff6fc4b18ad7ab619c185fc66da8a0492e4b0d52
Ilari Junnonen <ilari.junnonen@solita.fi>
2025-01-13 15:36:58 +0200

    DPO-2645 update is_referenced_by when references are made or removed

--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/validators/dataset_validators.py
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/validators/dataset_validators.py
@@ -1,18 +1,21 @@
+import json
+import logging
 from typing import Any
 
-from ckan.types import Context
-from ckan.lib.navl.dictization_functions import Invalid
+import phonenumbers
 from ckan.common import _, config
-
+from ckan.lib.navl.dictization_functions import Invalid
+from ckan.logic import get_action
+from ckan.types import Context
 from ckanext.digitraffic_theme.helpers.helpers import is_dataset_public
-
 from ckanext.digitraffic_theme.model.mobility_theme import (
     MobilityTheme,
     MobilityThemeSub,
     is_valid_mobility_theme_sub,
 )
 from ckanext.digitraffic_theme.model.spatial_reference import SpatialReference
-import phonenumbers
+
+logger = logging.getLogger(__name__)
 
 
 def mobility_theme_sub_validator(key, data, errors, context):
@@ -53,8 +56,94 @@ def spatial_reference_validator(value: Any, context: Context):
     return value
 
 
+def dataset_reference_validator(value: Any, context: Context):
+    # get currently edited package from contect
+    package = context.get("package", None)
+
+    # in this case related_resource was set to empty for currently edited dataset
+    if package and not value:
+
+        # get dataset details
+        referring_dataset = get_action("package_show")(
+            context,
+            {"id": package.id},
+        )
+
+        resource_reference = referring_dataset.get("related_resource")
+
+        # a reference to another dataset exists if the below is true
+        if resource_reference and isinstance(resource_reference, str):
+
+            # the output validator for dataset references will transform the id into an url
+            # where the last part of the path is the id
+            referred_id = resource_reference.split("/")[-1]
+
+            # get the details of the referred dataset
+            referred_dataset = get_action("package_show")(
+                context,
+                {"id": referred_id},
+            )
+            reference_list = json.loads(referred_dataset["is_referenced_by"])
+
+            # remove the previous reference if it was found
+            if package.id in reference_list:
+                logger.info(
+                    f"Removing reference to dataset id {package.id} from dataset id {referred_id}"
+                )
+                reference_list.remove(package.id)
+                referred_dataset["is_referenced_by"] = json.dumps(reference_list)
+                # commit the changes
+                get_action("package_update")(context, referred_dataset)
+
+    # in this case related_resource was given a value for currently edited dataset
+    if package and value:
+        referred_dataset = get_action("package_show")(
+            context,
+            {"id": value},
+        )
+        # dataset being referred to by the currently edited dataset already has a list of references
+        if (
+            package
+            and referred_dataset.get("is_referenced_by")
+            and isinstance(referred_dataset["is_referenced_by"], str)
+        ):
+            # values are saved to the database as strings of json lists but the output validator transforms these to strings
+            # where the list elements are separated by line breaks
+            reference_list = referred_dataset["is_referenced_by"].split("\n")
+
+            if package.id in reference_list:
+                pass
+            elif package.id is not None:
+                reference_list.append(package.id)
+                referred_dataset["is_referenced_by"] = json.dumps(reference_list)
+        # dataset being referred to by the currently edited dataset does not have previous references, create a new list
+        else:
+            referred_dataset["is_referenced_by"] = json.dumps([package.id])
+
+        # commit the changes
+        get_action("package_update")(context, referred_dataset)
+
+    return value
+
+
 def dataset_reference_output_validator(value: Any, context: Context):
     ckan_site_url = config.get("ckan.site_url", "").rstrip("/")
+    # show reference to public datasets
     if is_dataset_public(value):
         return f"{ckan_site_url}/dataset/{value}"
+    return ""
+
+
+def dataset_referenced_by_output_validator(value: Any, context: Context):
+    if value and isinstance(json.loads(value), list):
+        ckan_site_url = config.get("ckan.site_url", "").rstrip("/")
+        dataset_ids = json.loads(value)
+        # show references made by public datasets
+        # TODO: format strings in html templates to avoid string manipulation in validator functions
+        public_datasets = [
+            f"{ckan_site_url}/dataset/{dataset_id}"
+            for dataset_id in dataset_ids
+            if is_dataset_public(dataset_id)
+        ]
+        return "\n".join(public_datasets)
     return ""
