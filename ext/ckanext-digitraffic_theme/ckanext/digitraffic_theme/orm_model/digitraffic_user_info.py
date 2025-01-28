730307b09f19cb5a5919f0fe7d4a4f2c6168dd6c
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-01-28 16:32:23 +0200

    WIP: Add user info table and the data to the user view. Also, fetch the data from the newly created db to the form

---
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/orm_model/digitraffic_user_info.py
@@ -0,0 +1,73 @@
+from typing import TypedDict, NotRequired, Self, Optional
+
+from ckan.plugins import toolkit
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship
+from ckan.model import meta
+
+class DigitrafficUserInfoInput(TypedDict):
+    phone: NotRequired[str]
+    first_name: NotRequired[str]
+    surname: NotRequired[str]
+    country_of_residence: NotRequired[str]
+    county: NotRequired[str]
+    post_code: NotRequired[str]
+    city: NotRequired[str]
+    street_address: NotRequired[str]
+
+class DigitrafficUserInfo(toolkit.BaseModel):
+    __tablename__ = 'digitraffic_user_info'
+    id = sa.Column('id', sa.Integer, primary_key=True)
+    user_id = sa.Column('user', sa.Text, unique=True, nullable=False)
+    phone = sa.Column('phone', sa.Text)
+    first_name = sa.Column('first_name', sa.Text)
+    surname = sa.Column('surname', sa.Text)
+    country_of_residence = sa.Column('country_of_residence', sa.Text)
+    county = sa.Column('county', sa.Text)
+    post_code = sa.Column('post_code', sa.Text)
+    city = sa.Column('city', sa.Text)
+    street_address = sa.Column('street_address', sa.Text)
+
+    ## Relationship without using a foreign key
+    #user_id_rel = relationship('User',
+    #                           foreign_keys=[user_id],
+    #                              primaryjoin='User.id == DigitrafficUserInfo.user_id')
+
+    def __init__(self, user_id: str, phone: str, first_name: str, surname: str, country_of_residence: str,
+                 county: str, post_code: str, city: str, street_address: str):
+        self.user_id = user_id
+        self.phone = phone
+        self.first_name = first_name
+        self.surname = surname
+        self.country_of_residence = country_of_residence
+        self.county = county
+        self.post_code = post_code
+        self.city = city
+        self.street_address = street_address
+
+    @classmethod
+    def get(cls, user_id: str) -> Self:
+        return (meta.Session.query(cls)
+                .filter(cls.user_id == user_id)
+                .first())
+
+    @classmethod
+    def update(cls, user_id: str, user_data: DigitrafficUserInfoInput):
+        raise NotImplementedError('User Info update is not implemented yet!')
+
+    @classmethod
+    def create(cls, user_id: str, user_data: DigitrafficUserInfoInput):
+        user_info = cls(
+            user_id,
+            user_data.get("phone"),
+            user_data.get("first_name"),
+            user_data.get("surname"),
+            user_data.get("country_of_residence"),
+            user_data.get("county"),
+            user_data.get("post_code"),
+            user_data.get("city"),
+            user_data.get("street_address")
+        )
+        meta.Session.add(user_info)
+        meta.Session.commit()
+        return user_info
--- ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/plugin.py
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/plugin.py
@@ -1,7 +1,12 @@
 # encoding: utf-8
+import pprint
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from typing import Callable
 from ckan.lib.plugins import DefaultTranslation
+from ckan.types import ActionResult, Context, DataDict
+from ckanext.digitraffic_theme.views.user import DigitrafficEditView
+from ckanext.digitraffic_theme.orm_model.digitraffic_user_info import DigitrafficUserInfo
 
 from ckanext.digitraffic_theme.validators.dataset_validators import (
     mobility_theme_sub_validator,
@@ -9,6 +14,86 @@ from ckanext.digitraffic_theme.validators.dataset_validators import (
     spatial_reference_validator,
 )
 from ckanext.digitraffic_theme.helpers.helpers import helpers
+from flask import Blueprint
+
+digitraffic_blueprint = Blueprint("digitraffic", __name__, template_folder="templates")
+
+@toolkit.chained_action
+def user_show(original_action: Callable, context: Context, data_dict: DataDict) -> ActionResult.UserShow:
+    """
+    Adds digitraffic user info to the user data returned by `user_show` action.
+
+    Relies on the CKAN default action to add `user_obj` into context.
+
+    TODO: The default authorization is to let any logged-in user to see user info. Should we keep this?
+    """
+    print("####################### user_show #####################")
+    user_data = original_action(context, data_dict)
+
+    user_id = context['user_obj'].id
+    digitraffic_user_info = DigitrafficUserInfo.get(user_id)
+
+    print(f'user_id: {user_id}')
+    print('digitraffic_user_info')
+    pprint.pprint(digitraffic_user_info)
+
+    digitraffic_user = {
+        'digitraffic_user': {
+            'phone': getattr(digitraffic_user_info, 'phone', None),
+            'first_name': getattr(digitraffic_user_info, 'first_name', None),
+            'surname': getattr(digitraffic_user_info, 'surname', None),
+            'country_of_residence': getattr(digitraffic_user_info, 'country_of_residence', None),
+            'county': getattr(digitraffic_user_info, 'county', None),
+            'post_code': getattr(digitraffic_user_info, 'post_code', None),
+            'city': getattr(digitraffic_user_info, 'city', None),
+            'street_address': getattr(digitraffic_user_info, 'street_address', None)
+        }
+    }
+    return (user_data or {}) | digitraffic_user
+
+@toolkit.chained_action
+def user_update(original_action: Callable, context: Context, data_dict: DataDict) -> ActionResult.UserShow:
+    """
+    Updates digitraffic user info when user data is updated.
+
+    Relies on the CKAN default action to add `user_obj` into context.
+    """
+    print("####################### user_update #####################")
+    user_data = original_action(context, data_dict)
+    updated_user_id = context['user_obj'].id
+
+    print(f'updated_user_id: {updated_user_id}')
+    print('data_dict')
+    pprint.pprint(data_dict)
+    is_user_info_updated = DigitrafficUserInfo.get(updated_user_id) is not None
+    user_info_input_data = {'phone': data_dict.get('phone'),
+                       'first_name': data_dict.get('first_name'),
+                       'surname': data_dict.get('surname'),
+                       'country_of_residence': data_dict.get('country_of_residence'),
+                       'county': data_dict.get('county'),
+                       'post_code': data_dict.get('post_code'),
+                       'city': data_dict.get('city'),
+                       'street_address': data_dict.get('street_address')}
+    if is_user_info_updated:
+        digitraffic_user_info = DigitrafficUserInfo.update(updated_user_id, user_info_input_data)
+    else:
+        digitraffic_user_info = DigitrafficUserInfo.create(updated_user_id, user_info_input_data)
+    digitraffic_user = {
+        'digitraffic_user': {
+            'phone': getattr(digitraffic_user_info, 'phone', None),
+            'first_name': getattr(digitraffic_user_info, 'first_name', None),
+            'surname': getattr(digitraffic_user_info, 'surname', None),
+            'country_of_residence': getattr(digitraffic_user_info, 'country_of_residence', None),
+            'county': getattr(digitraffic_user_info, 'county', None),
+            'post_code': getattr(digitraffic_user_info, 'post_code', None),
+            'city': getattr(digitraffic_user_info, 'city', None),
+            'street_address': getattr(digitraffic_user_info, 'street_address', None)
+        }
+    }
+    return (user_data or {}) | digitraffic_user
+    #user_data = original_action(context, data_dict)
+    #return (user_data or {}) | digitraffic_user
+
 
 
 class DigitrafficThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
@@ -17,6 +102,8 @@ class DigitrafficThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IValidators)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.IActions)
 
     # Declare that this class implements IConfigurer.
     plugins.implements(plugins.IConfigurer)
@@ -42,6 +129,18 @@ class DigitrafficThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
         # templates.
         toolkit.add_resource("assets", "digitraffic_theme")
         toolkit.add_resource("assets", "digitraffic_web_component")
+
+    def get_blueprint(self):
+        #_edit_view = DigitrafficEditView.as_view('edit')
+        #digitraffic_blueprint.add_url_rule('/user/edit', view_func=_edit_view)
+        #digitraffic_blueprint.add_url_rule('/user/edit/<id>', view_func=_edit_view)
+        return [digitraffic_blueprint]
+
+    def get_actions(self):
+        return {
+            'user_show': user_show,
+            'user_update': user_update
+        }
 
     def get_helpers(self):
         return helpers
