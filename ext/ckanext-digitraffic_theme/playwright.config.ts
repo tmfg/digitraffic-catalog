40bad3193be4d4b2b2bc168a04dac667c6cfb023
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-02-27 15:24:55 +0200

    Add Playwright env variables to it's own file as well

--- ext/ckanext-digitraffic_theme/playwright.config.ts
+++ ext/ckanext-digitraffic_theme/playwright.config.ts
@@ -3,12 +3,10 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-if (process.env.CI) {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  // Read environmental variables from file
-  dotenv.config({path: path.resolve(__dirname, '.env.playwright')});
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Read environmental variables from file
+dotenv.config({path: path.resolve(__dirname, '.env.playwright')});
 
 export default defineConfig({
   testDir: './e2e',
