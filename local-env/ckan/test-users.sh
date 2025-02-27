7482c3da93b38af1cffc4b931fe5e64845075820
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-02-27 10:13:23 +0200

    Add E2E environment variables to CI pipeline

--- local-env/ckan/test-users.sh
+++ local-env/ckan/test-users.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ -n "${SYSADMIN_ID}" ]];
+if [[ -n "${E2E_SYSADMIN_ID}" ]];
 then
     INSERT_ADMIN_STATEMENT="INSERT INTO public.user (id, name, email, fullname, sysadmin, created) \
-VALUES ('${SYSADMIN_ID}', '${SYSADMIN_NAME}', '${SYSADMIN_EMAIL}', '${SYSADMIN_FULLNAME}', true, NOW());";
+VALUES ('${E2E_SYSADMIN_ID}', '${E2E_SYSADMIN_NAME}', '${E2E_SYSADMIN_EMAIL}', '${E2E_SYSADMIN_FULLNAME}', true, NOW());";
 
     psql -c "${INSERT_ADMIN_STATEMENT}" "${CKAN_SQLALCHEMY_URL}";
 fi
