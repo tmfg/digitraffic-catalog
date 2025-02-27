663024c8210c90f60cb92785389bcfd4271250e9
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-02-27 14:01:14 +0200

    Add config back

--- local-env/ckan/dev-setup.sh
+++ local-env/ckan/dev-setup.sh
@@ -5,3 +5,7 @@ ckan config-tool "${CKAN_INI}" -s DEFAULT "debug = true"
 ckan config-tool "${CKAN_INI}" -s logger_ckan "level = DEBUG"
 ckan config-tool "${CKAN_INI}" -s logger_ckanext "level = DEBUG"
 
+ckan config-tool "${CKAN_INI}" "ckan.auth.user_create_groups = false"
+ckan config-tool "${CKAN_INI}" "ckan.auth.user_create_organizations = false"
+ckan config-tool "${CKAN_INI}" "ckan.auth.user_delete_groups = false"
+ckan config-tool "${CKAN_INI}" "ckan.auth.user_delete_organizations = false"
