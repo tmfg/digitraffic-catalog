3d0a1791a2a161d424f6c39816e671538c4bcdb4
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-02-27 17:54:54 +0200

    Fix issues

--- local-env/wait-for-containers.sh
+++ local-env/wait-for-containers.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 TOTAL_SLEEP=0;
 while [ "$(docker inspect --format='{{.State.Health.Status}}' datakatalogi-local-ckan-1)" != "healthy" ];
     do sleep 1;
-    ((TOTAL_SLEEP++));
+    TOTAL_SLEEP=$((TOTAL_SLEEP+1));
     if (( TOTAL_SLEEP % 60 == 0 )); then
         echo "ENV VARIABLES:";
         docker inspect --format='{{range .Config.Env }}{{println (index (split . "=") 0)}}{{end}}' datakatalogi-local-ckan-1;
