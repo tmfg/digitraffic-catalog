15c94ea5e9c66258e751042468287b793ae3f595
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-03-03 16:10:16 +0200

    Set a default value if E2E_SYSADMIN_ID environment variable is not set

--- local-env/ckan/test-users.sh
+++ local-env/ckan/test-users.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -n "${E2E_SYSADMIN_ID}" ]; then
+if [ -n "${E2E_SYSADMIN_ID:-}" ]; then
     INSERT_ADMIN_STATEMENT="INSERT INTO public.user (id, name, email, fullname, sysadmin, created) VALUES ('${E2E_SYSADMIN_ID}', '${E2E_SYSADMIN_NAME}', '${E2E_SYSADMIN_EMAIL}', '${E2E_SYSADMIN_FULLNAME}', true, NOW());";
 
     psql -c "${INSERT_ADMIN_STATEMENT}" "${CKAN_SQLALCHEMY_URL}";
