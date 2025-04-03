7199ccdd2fcb398fd778ef60cf20e9f0c73ce777
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-04-03 23:32:37 +0300

    OpenTelemetry tracing data into Jaeger

--- local-env/ckan/dev-setup.sh
+++ local-env/ckan/dev-setup.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 ckan config-tool "${CKAN_INI}" -s DEFAULT "debug = true"
 ckan config-tool "${CKAN_INI}" -s logger_ckan "level = DEBUG"
 ckan config-tool "${CKAN_INI}" -s logger_ckanext "level = DEBUG"
+ckan config-tool "${CKAN_INI}" -s formatter_generic 'format = '\''{"asctime": %(asctime)s, "name": %(name)s, "levelname": %(levelname)s, "message": %(message)s}'\'''
 
 ckan config-tool "${CKAN_INI}" "ckan.auth.user_create_groups = false"
 ckan config-tool "${CKAN_INI}" "ckan.auth.user_create_organizations = false"
