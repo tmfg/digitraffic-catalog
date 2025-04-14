7b5877e4c1698c855d67ae21bcb48a6cb4f3f703
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-04-14 11:53:14 +0300

    Do manual OpenTelemetry instrumentation for CKAN and do it in it's own plugin

--- local-env/ckan/dev-setup.sh
+++ local-env/ckan/dev-setup.sh
@@ -4,16 +4,6 @@ set -euo pipefail
 ckan config-tool "${CKAN_INI}" -s DEFAULT "debug = true"
 ckan config-tool "${CKAN_INI}" -s logger_ckan "level = DEBUG"
 ckan config-tool "${CKAN_INI}" -s logger_ckanext "level = DEBUG"
-ckan config-tool "${CKAN_INI}" -s formatter_generic 'format = '\''{"asctime": ${asctime}, "name": ${name}, "levelname": ${levelname}, "message": ${message}, "SpanID": ${otelSpanID}, "TraceID": ${otelTraceID}, "otelServiceName": ${otelServiceName}, "otelTraceSampled": ${otelTraceSampled}}'\'''
-ckan config-tool "${CKAN_INI}" -s formatter_generic "style = $"
-ckan config-tool "${CKAN_INI}" -s formatter_generic "class = catalog_log_config.CustomFormatter"
-ckan config-tool "${CKAN_INI}" -s handlers "keys = console"
-ckan config-tool "${CKAN_INI}" -s logger_root "handlers = console"
-ckan config-tool "${CKAN_INI}" -s logger_ckan "handlers = console"
-ckan config-tool "${CKAN_INI}" -s logger_ckanext "handlers = console"
-ckan config-tool "${CKAN_INI}" -s logger_werkzeug "handlers = console"
-
-ckan config-tool "${CKAN_INI}" -s handler_console "formatter = generic"
 
 ckan config-tool "${CKAN_INI}" "ckan.auth.user_create_groups = false"
 ckan config-tool "${CKAN_INI}" "ckan.auth.user_create_organizations = false"
