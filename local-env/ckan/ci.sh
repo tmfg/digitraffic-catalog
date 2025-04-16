0ff4b7e31959b48ce7c69ad328011da931079ad2
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-04-16 13:27:08 +0300

    Disable OpenTelemetry plugin for CI

---
+++ local-env/ckan/ci.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$CI" != true ]; then
+  exit 0
+fi
+
+echo "CI environment variable is set to true. Disabling opentelemetry"
+
+CKAN__CI_PLUGINS=$(echo "${CKAN__PLUGINS}" | sed 's/digitraffic_opentelemetry//g')
+
+ckan config-tool ${CKAN_INI} "ckan.plugins = ${CKAN__PLUGINS}"
