8fcc0ceebcbd621dd753a4917d0e6779e202aedc
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-04-16 12:25:57 +0300

    Instrument Redis and SQLAlchemy. Add user oid to the baggage.

--- ext/ckanext-digitraffic_opentelemetry/ckanext/digitraffic_opentelemetry/plugin.py
+++ ext/ckanext-digitraffic_opentelemetry/ckanext/digitraffic_opentelemetry/plugin.py
@@ -1,22 +1,25 @@
 import os
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.model.meta import engine
+from typing import Optional
 from opentelemetry import trace, context, baggage
 from opentelemetry import propagate
+#from opentelemetry.context import Context
 from opentelemetry.sdk.resources import (
     SERVICE_NAME,
     SERVICE_NAMESPACE,
     Resource,
     get_aggregated_resources
 )
-from opentelemetry.sdk.trace import TracerProvider, SpanProcessor
+from opentelemetry.sdk.trace import TracerProvider, Span
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
+    SpanProcessor
 )
 from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
-#from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 from opentelemetry.propagators import textmap
 from opentelemetry.baggage.propagation import W3CBaggagePropagator
 
@@ -28,8 +31,21 @@ from opentelemetry.propagators.composite import CompositePropagator
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.urllib3 import URLLib3Instrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
+from opentelemetry.instrumentation.redis import RedisInstrumentor
 from opentelemetry.processor.baggage import BaggageSpanProcessor, ALLOW_ALL_BAGGAGE_KEYS
 
+
+def _add_user_to_baggage(span: Span, flask_request_environ):
+    """
+    Add the user ID to the baggage of the span. This is used to propagate the user ID.
+    """
+    user_id_attribute = "user.oid"
+    if span.is_recording():
+        # Set the user ID as an attribute to the currently running span
+        span.set_attribute(user_id_attribute, toolkit.g.get('user'))
+    new_context = baggage.set_baggage(user_id_attribute, toolkit.g.get('user'))
+    context.attach(new_context)
 
 class DigitrafficOpentelemetryPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IMiddleware)
@@ -38,8 +54,10 @@ class DigitrafficOpentelemetryPlugin(plugins.SingletonPlugin):
         self._configure_traces()
         self._propagate_trace_headers()
         self._add_trace_to_logging()
-        self._add_WSGI_instrumentation(app)
+        self._add_flask_instrumentation(app)
         self._add_request_instrumentation()
+        self._add_sqlalchemy_instrumentation()
+        self._add_redis_instrumentation()
         return app
 
     def make_error_log_middleware(self, app, config):
@@ -52,7 +70,7 @@ class DigitrafficOpentelemetryPlugin(plugins.SingletonPlugin):
         resource = self._get_resource_attributes()
 
         provider = TracerProvider(resource=resource, id_generator=AwsXRayIdGenerator())
-        self._add_baggage_to_attributes(provider)
+        self._add_baggage_to_span_attributes(provider)
         self._send_traces_to_collector(provider)
 
         # Sets the global default tracer provider
@@ -93,13 +111,13 @@ class DigitrafficOpentelemetryPlugin(plugins.SingletonPlugin):
         """
         LoggingInstrumentor().instrument(set_logging_format=True)
 
-    def _add_WSGI_instrumentation(self, app):
+    def _add_flask_instrumentation(self, app):
         """
         Instruments the Flask requests. Also, is responsible for extracting the request context from the incoming request.
         It does so by using propagators. The propagators are given the whole Flask request environment to pick the
         correct headers from.
         """
-        FlaskInstrumentor().instrument_app(app, enable_commenter=False, commenter_options={})
+        FlaskInstrumentor().instrument_app(app, enable_commenter=False, commenter_options={}, request_hook=_add_user_to_baggage)
 
     def _add_request_instrumentation(self):
         """
@@ -109,7 +127,7 @@ class DigitrafficOpentelemetryPlugin(plugins.SingletonPlugin):
         URLLib3Instrumentor().instrument()
         RequestsInstrumentor().instrument()
 
-    def _add_baggage_to_attributes(self, provider: TracerProvider):
+    def _add_baggage_to_span_attributes(self, provider: TracerProvider):
         baggage_processor = BaggageSpanProcessor(ALLOW_ALL_BAGGAGE_KEYS)
         provider.add_span_processor(baggage_processor)
 
@@ -120,6 +138,17 @@ class DigitrafficOpentelemetryPlugin(plugins.SingletonPlugin):
         processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=otel_endpoint))
         provider.add_span_processor(processor)
 
+    def _add_sqlalchemy_instrumentation(self):
+        """
+        Instrument the SQLAlchemy library to automatically create spans for SQL queries.
+        """
+        SQLAlchemyInstrumentor().instrument(engine=engine)
+
+    def _add_redis_instrumentation(self):
+        """
+        Instrument the Redis library to automatically create spans for Redis queries.
+        """
+        RedisInstrumentor().instrument()
 
 
 class W3CExtractorPropagator(TraceContextTextMapPropagator):
