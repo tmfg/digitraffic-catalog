795c7aab9bc65fd71564d35cca569313ed279f2d
Ilari Junnonen <ilari.junnonen@solita.fi>
2025-04-29 13:47:57 +0300

    DPO-3143 display input field and hide button on render if previous value exists

--- ext/ckanext-digitraffic_theme/src/ts/modules/language-toggle-buttons.ts
+++ ext/ckanext-digitraffic_theme/src/ts/modules/language-toggle-buttons.ts
@@ -8,26 +8,31 @@ const LanguageToggleButtons: ckan.Module<HTMLDivElement> = {
     const toggleButtons = this._getToggleButtons();
     toggleButtons.children(".language-toggle-button").each((_index, element) => {
       const button = $(element);
-      const buttonId = button.attr("id")
+      const buttonId = button.attr("id");
 
+      // toggle visibility of input field when language toggle button is clicked
       button.on("click", (event) => {
         event.preventDefault();
-        const inputField = this.$(`#field-${buttonId}`)
+        const inputField = this.$(`#field-${buttonId}`);
         inputField.parent().parent().removeClass("hidden");
-        button.hide();
+        button.addClass("hidden");
       });
     });
 
     const closeButtons = this._getCloseButtons();
     closeButtons.each((_index, element) => { 
       const button = $(element);
-      const buttonId = button.attr("id")
+      const buttonId = button.attr("id");
       
+      // hide input field when related close button is clicked 
       button.on("click", (event) => {
         event.preventDefault();
-        const toggleButton = $(`#${buttonId}.language-toggle-button`)
-        toggleButton.show();
+        const toggleButton = $(`#${buttonId}.language-toggle-button`);
+        toggleButton.removeClass("hidden");
         button.parent().parent().addClass("hidden");
+        const inputField = this.$(`#field-${buttonId}`);
+        // clear value of input field when button is clicked and field is hidden
+        inputField.val("");
       });
        
   });
