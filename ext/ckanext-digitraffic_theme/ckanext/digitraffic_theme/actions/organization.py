89bf404b1cec344a41b1427ab019dcf57f91c3f5
Ilari Junnonen <ilari.junnonen@solita.fi>
2025-08-20 15:58:59 +0300

    DPO-3354 drop organization/group extras before create/update

---
+++ ext/ckanext-digitraffic_theme/ckanext/digitraffic_theme/actions/organization.py
@@ -0,0 +1,25 @@
+import ckan.plugins.toolkit as toolkit
+from typing import Callable
+from ckan.types import Context, DataDict, ActionResult
+
+
+# Override organization_create to drop extras (custom_fields in the templates)
+@toolkit.chained_action
+def organization_create(
+    original_action: Callable, context: Context, data_dict: DataDict
+) -> ActionResult.OrganizationCreate:
+    if "extras" in data_dict:
+        data_dict.pop("extras", None)
+
+    return original_action(context, data_dict)
+
+
+# Override organization_update to drop extras (custom_fields in the templates)
+@toolkit.chained_action
+def organization_update(
+    original_action: Callable, context: Context, data_dict: DataDict
+) -> ActionResult.OrganizationUpdate:
+    if "extras" in data_dict:
+        data_dict.pop("extras", None)
+
+    return original_action(context, data_dict)
